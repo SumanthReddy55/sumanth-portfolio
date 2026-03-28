a8ee7d50fc77aaf30275145f216b80d5d14abb64
sumanth reddy <47923765+SumanthReddy55@users.noreply.github.com>
2026-03-28 17:48:36 -0500

    Update splitText.ts

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -13,14 +13,17 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 export default function setSplitText() {
   ScrollTrigger.config({ ignoreMobileResize: true });
   if (window.innerWidth < 900) return;
+
   const paras: NodeListOf<ParaElement> = document.querySelectorAll(".para");
   const titles: NodeListOf<ParaElement> = document.querySelectorAll(".title");
 
   const TriggerStart = window.innerWidth <= 1024 ? "top 60%" : "20% 60%";
   const ToggleAction = "play pause resume reverse";
 
+  // ✅ PARAGRAPHS
   paras.forEach((para: ParaElement) => {
     para.classList.add("visible");
+
     if (para.anim) {
       para.anim.progress(1).kill();
       para.split?.revert();
@@ -31,50 +34,56 @@ export default function setSplitText() {
       linesClass: "split-line",
     });
 
+    const words = para.split?.words || [];
+
     para.anim = gsap.fromTo(
-      para.split.words,
+      words,
       { autoAlpha: 0, y: 80 },
       {
         autoAlpha: 1,
+        y: 0,
+        duration: 1,
+        ease: "power3.out",
+        stagger: 0.02,
         scrollTrigger: {
-          trigger: para.parentElement?.parentElement,
+          trigger: para,
           toggleActions: ToggleAction,
           start: TriggerStart,
         },
-        duration: 1,
-        ease: "power3.out",
-        y: 0,
-        stagger: 0.02,
       }
     );
   });
+
+  // ✅ TITLES
   titles.forEach((title: ParaElement) => {
     if (title.anim) {
       title.anim.progress(1).kill();
       title.split?.revert();
     }
+
     title.split = new SplitText(title, {
       type: "chars,lines",
       linesClass: "split-line",
     });
+
+    const chars = title.split?.chars || [];
+
     title.anim = gsap.fromTo(
-      title.split.chars,
+      chars,
       { autoAlpha: 0, y: 80, rotate: 10 },
       {
         autoAlpha: 1,
+        y: 0,
+        rotate: 0,
+        duration: 0.8,
+        ease: "power2.inOut",
+        stagger: 0.03,
         scrollTrigger: {
-          trigger: title.parentElement?.parentElement,
+          trigger: title,
           toggleActions: ToggleAction,
           start: TriggerStart,
         },
-        duration: 0.8,
-        ease: "power2.inOut",
-        y: 0,
-        rotate: 0,
-        stagger: 0.03,
       }
     );
   });
-
-  ScrollTrigger.addEventListener("refresh", () => setSplitText());
 }
